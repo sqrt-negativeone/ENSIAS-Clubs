ab0a4bfbb9fda6d6e86cc168c2f0749150b19f8a
moadfakhri <mouad_fakhri@um5.ac.ma>
2020-05-12 14:35:04 +0000

    implement the javascript logic for new cellule and new event form submission buttons

--- moad-work/pages/assets/js/join.js
+++ moad-work/pages/assets/js/join.js
@@ -11,8 +11,38 @@ $(document).ready(()=>{
             function (data){
                 btn.removeClass('btn-primary');
                 btn.addClass('btn-secondary');
-                console.log(data);
+                alert("request sent seccessfully");
             })
         }
     })
 })
+
+function new_cellule(){
+    var form=$("#cel_form");
+    form.submit(function(e){
+        e.preventDefault();
+        var f=$(this);
+        var url="../functions/create_cellule.php";
+        $.post(url,f.serialize(),function(){
+            if (!alert('cellule created successfully')){
+                window.location.reload();
+            }
+        });
+    })
+}
+
+function new_event(){
+    var form=$("#event_form");
+    form.submit(function(e){
+        e.preventDefault();
+        var f=$(this);
+        console.log(f.serialize());
+        
+        var url="../functions/create_event.php";
+        $.post(url,f.serialize(),function(){
+            if (!alert('cellule created successfully')){
+                window.location.reload();
+            }
+        });
+    })
+}
