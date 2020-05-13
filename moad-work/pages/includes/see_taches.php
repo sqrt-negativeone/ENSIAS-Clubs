01cb1f3f566f5a9467ae7153ecba8ab88129dec2
moadfakhri <mouad_fakhri@um5.ac.ma>
2020-05-13 16:13:24 +0000

    implement the logic for searching members to see their progress

--- moad-work/pages/includes/see_taches.php
+++ moad-work/pages/includes/see_taches.php
@@ -7,11 +7,10 @@
                     <div class="col">
                         <div class="card" style="margin-bottom: 30px;">
                             <div class="card-body" style="padding-bottom: 0;">
-                                <!--TODO: implement the search feature-->
                                 <form>
                                     <div class="form-group">
                                         <div class="form-row">
-                                            <div class="col"><input class="form-control" type="search" placeholder="Search">
+                                            <div class="col"><input class="form-control" id="srch_mem" type="text" placeholder="Search" onkeyup="search_mem()">
                                             </div>
                                         </div>
                                     </div>
@@ -22,27 +21,8 @@
                 </div>
                 <hr>
                 <div class="row">
-                    <div class="col">
-                        <?php
-                        $nb_mem = 1;
-                        for ($i = 0; $i < $nb_mem; $i++) {
-                            $username="username";
-                            $avatar="assets/img/avatars/avatar1.jpeg";
-                        ?>
-                            <div class="card shadow border-left-primary py-2" data-aos="zoom-in-up" data-toggle="modal" data-target="#modal1" type="button" style="margin-bottom: 25px;width: 250px;margin-right: auto;margin-left: auto;">
-                                <div class="card-body" style="padding-top: 5px;padding-bottom: 5px;padding-right: 5px;padding-left: 5px;">
-                                    <div class="row align-items-center no-gutters">
-                                        <div class="col-auto">
-                                            <img class="border rounded-circle img-profile" src=<?php echo htmlspecialchars($avatar) ?>></div>
-                                        <div class="col mr-2">
-                                            <div class="text-uppercase text-primary font-weight-bold text-xs mb-1" style="margin-left: 5px;">
-                                                <span style="font-size: 120%;margin-left: 5px;"><?php echo htmlspecialchars($username) ?></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php } ?>
+                    <div class="col" id="srch_out">
+                        <?php include 'functions/search_mem.php'?>
                     </div>
                 </div>
             </div>
