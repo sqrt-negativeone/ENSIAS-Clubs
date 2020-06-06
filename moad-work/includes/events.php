d98a7fa9650c090f62372a38dded56e273d63b93
maftah1998 <58748924+maftah1998@users.noreply.github.com>
2020-06-06 11:07:17 +0100

    Fix events query and Carousel items
    Only the first carousel item should be active.

--- moad-work/includes/events.php
+++ moad-work/includes/events.php
@@ -1,29 +1,58 @@
 <?php
-$sql = $pdo->query("select * from evenement where photo is not null and date_fin>getdate() ordered by date_fin ASC");
-$resault = $sql->fetchAll();
-$n = $resault->rowCount();
+	//GET EVENTS
+	$nature = 'PU';
+	$sql_events = "select * from evenement where nature = ? and date_deb>now() or (now()>date_deb and now()<date_fin) order by date_fin ASC";
+	$stmt_events = $pdo -> prepare($sql_events);
+	$stmt_events ->execute([$nature]);
+	$events = $stmt_events -> fetchAll();
+    $n = count($events);
 ?>
 
 <section class="clean-block slider dark">
     <div class="container">
         <div class="block-heading">
-            <h2 class="text-info">EVENEMENTS</h2>
+            <h2 class="text-info">ÉVÈNEMENTS</h2>
             <p style="font-size: 20px;">D&eacute;couvrez nos prochains &eacute;v&eacute;nements :</p>
         </div>
         <?php
         if ($n > 0) {
         ?>
             <div class="carousel slide" data-ride="carousel" id="carousel-1">
-                <div class="carousel-inner" role="listbox">
-                    <?php
-                    foreach ($resault as $event) {
-                        $event_title = $event['titre'];
-                        $discr = $event['descreption'];
-                        $date = $event['date_fin'];
-                        $pic = $event['photo'];
+                   <?php
+                    $event_title = $events[0]['titre'];
+                    $discr = $events[0]['descr_event'];
+                    $date = $events[0]['date_fin'];
+
+                    if ($events[0]['photo'] != '') {
+                        $pic = "data:image/*;base64,".base64_encode($events[0]['photo']);
+                    }else{
+                        $pic = "img/profile.png";
+                    }
                     ?>
-                        <div class="carousel-item active">
-                            <img class="w-100 d-block" src=<?php echo htmlspecialchars($pic) ?> alt="Event Image">
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src=<?php echo htmlspecialchars($pic) ?> alt="Event Image">
+                        <div class="text-center">
+                            <h2 class="text-center"><?php echo htmlspecialchars($event_title) ?></h2>
+                            <h6><?php echo htmlspecialchars($date) ?></h6>
+                            <p><br><?php echo htmlspecialchars($discr) ?><br><br></p>
+                        </div>
+                    </div>
+                    <?php
+                    for($i=1; $i<$n; $i++) {
+                        $event_title = $events[$i]['titre'];
+                        $discr = $events[$i]['descr_event'];
+                        $date = $events[$i]['date_fin'];
+
+                        if ($events[$i]['photo'] != '') {
+                            $pic = "data:image/*;base64,".base64_encode($events[$i]['photo']);
+                        }else{
+                            $pic = "img/profile.png";
+                        }
+                       
+                    ?>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src=<?php echo htmlspecialchars($pic) ?> alt="Event Image">
                             <div class="text-center">
                                 <h2 class="text-center"><?php echo htmlspecialchars($event_title) ?></h2>
                                 <h6><?php echo htmlspecialchars($date) ?></h6>
@@ -45,7 +74,7 @@ $n = $resault->rowCount();
                 <ol class="carousel-indicators">
                     <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
                     <?php
-                    for ($i = 1; $i < $n; $i++) {
+                    for ($i = 0; $i < $n; $i++) {
                         echo '<li data-target="#carousel-1" data-slide-to="' . $i . '"></li>';
                     }
                     ?>
